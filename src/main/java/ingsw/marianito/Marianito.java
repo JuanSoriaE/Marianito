4f8c96f7c6dc0451e444100b018a0a1c1d380ee1
JuanSoriaE <jsoriae1700@alumno.ipn.mx>
2024-12-09 20:47:56 -0600

    First commit

---
+++ src/main/java/ingsw/marianito/Marianito.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ingsw.marianito;
+
+/**
+ *
+ * @author juans
+ */
+public class Marianito {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
